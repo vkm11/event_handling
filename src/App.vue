ec7b791661f36d1fd2d569d1f1d43cc462b00582
vijaykumar <vijay1111mane@gmail.com>
2022-05-27 12:45:31 +0530

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>{{ name }}</h2>
+  <div>
+    <!-- <button v-on:click="name = 'Vijaykumar'" >Change Name</button> -->
+    <!-- <button v-on:mouseover="name = 'Vijaykumar'" >Change Name</button> -->
+    <button v-on:click="changeName">Change Name</button>
+  </div>
+    
+  <h2>{{count}}</h2>
+  <div>
+    <!-- <button v-on:click="count += 1">Increment</button>
+    <button v-on:click="count -= 1">Decrement</button> -->
+
+    <button @click="increment(1)">Increment 1</button>
+    <button @click="increment(5)">Increment 5</button>
+    <button v-on:click="decrement(1)">Decrement 1</button>
+    <button v-on:click="decrement(5)">Decrement 5</button>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{ 
+      name:'Vijay',
+      count: 0
+    }
+  },
+  methods:{ 
+    changeName(){ 
+      this.name = 'kumar'
+    },
+    // increment(){ 
+    //   this.count +=1
+    // },
+    // decrement(){ 
+    //   this.count -= 1
+    // }
+
+    increment(num){ 
+      this.count +=num
+    },
+    decrement(num){ 
+      this.count -= num
+    }
   }
+
 }
 </script>
 
